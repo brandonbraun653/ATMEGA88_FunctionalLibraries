5f3fa8b1a347db8b9e4a2b4f93bc710b8fa08c52
brandonbraun653 <brandonbraun653@gmail.com>
2016-07-20 13:45:27 -0600

    Add files via upload

---
+++ adc.cpp
@@ -0,0 +1,23 @@
+/*
+ * adc.cpp
+ *
+ * Created: 7/20/2016 12:43:42 PM
+ *  Author: bbraun
+ */ 
+ #include "adc.h"
+
+ /*!Pre-scales the ADC clock*/
+ void ADC_setPrescaler(uint8_t prescaler){
+	
+ }
+
+ /*!Selects ADC Input Channel
+
+ Selects a channel to be used as the input to the ADC. Only 
+ one channel can be used at a time. If another channel is 
+ selected while a conversion is in progress, the new channel
+ setting will take effect after the conversion is complete. 
+ */
+ void ADC_setMux(uint8_t muxSelect){
+	
+ }
---
+++ adc.h
@@ -0,0 +1,16 @@
+/*
+ * adc.h
+ *
+ * Created: 7/20/2016 12:43:55 PM
+ *  Author: bbraun
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+
+
+
+#endif /* ADC_H_ */
